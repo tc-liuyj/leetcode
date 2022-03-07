69f882631491ae1024787dbacb38a32c21968488
tc-liuyj <tc-liuyj@163.com>
2022-03-07 11:27:43 +0800

    init

---
+++ basic/src/test/java/com/tc_liuyj/AppTest.java
@@ -0,0 +1,20 @@
+package com.tc_liuyj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
